a47b32fab41ec4118732f5c450aee6a2ca184778
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-02-11 14:46:37 -0300

    feat: create Login component

--- app/web/src/pages/Login.tsx
+++ app/web/src/pages/Login.tsx
@@ -1,11 +1,25 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface Inputs {
+  user: String,
+  password: String,
+};
+
 export default function Login() {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const handleLogin: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  }
+
   return(
-    <form>
+    <form onSubmit={handleSubmit(handleLogin)}>
       <label htmlFor='id_user'>
         <input 
           type='text'
           placeholder='user'
           id='id_user'
+          {...register('user')}
         />
       </label>
       <label htmlFor='id_password'>
@@ -13,6 +27,7 @@ export default function Login() {
           type='text'
           placeholder='passaword'
           id='id_password'
+          {...register('password')}
         />
       </label>
       <button type='submit'>Send</button>
